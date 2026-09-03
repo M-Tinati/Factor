f5b507fbaf307481f201225f350047144d0897fe
M-Tinati <tinatigithub@gmail.com>
2026-09-03 19:29:26 +0330

    update

--- core.py
+++ core.py
@@ -1,22 +1,27 @@
-ProductCode = str(input("Enter Product Code :"))
-ProductName = str(input("Enter Product Name :"))
-ProductStock = int(input("Enter Product Stoke :"))
-ProductPrice = int(input("Enter Product Price :"))
+is_Active = 0
 Inventorys = []
-Inventory = {
+print("you should add 3 product information")
+
+while is_Active < 3:
+    ProductCode = str(input("Enter Product Code :"))
+    ProductName = str(input("Enter Product Name :"))
+    ProductStock = int(input("Enter Product Stoke :"))
+    ProductPrice = int(input("Enter Product Price :"))
+    Inventory = {
     "code" : ProductCode,
     "name" : ProductName,
     "stock" : ProductStock,
     "price" : ProductPrice,
-}
+    }
+    Inventorys.append(Inventory)
+    print(Inventorys)
+    is_Active += 1
 
-Inventorys.append(Inventory)
-print(Inventorys)
 for i in Inventorys:
     print("========== PRODUCTS ==========")
     print("Product Code :" , i["code"])
     print("Enter Product Name :" , i["name"])
     print("Enter Product Stock :" , i["stock"])
-    print("Enter Product Price :" , ProductPrice)
-    print(f"i : {i["stock"]} , \nAll price : {i["stock"]*i["price"]}")
+    print("Enter Product Price :" , i["price"])
+    print(f"product : {i['stock']} , \nAll price : {i['stock'] * i['price']}")
     print("==============================")
